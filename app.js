740f741515899a499f1a463662418bde7e79b777
antonio18099 <antoniosanchezr0921@gmial.com>
2024-08-03 08:52:55 -0500

    se han hecho  camios en el js

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+const frmEmail = document.getElementById('frm-email')
+fmrEmail.addEventListener('submit',senEmail)
+
+const serviceId = 'service_6agkihg'
+const templateId = 'template_lqbu7o9'
+const apikey = 'IfiAKkEoawMOm1Iw5'
+
+function senEmail(event) {
+    event.preventDefault()
+    emailjs.init(serviceId)
+
+    emailjs.sendForm(serviceId,templateId,frmEmail,apikey)
+    .then( result => Swal.fire('Su mensaje se ha enviado con exito.') )
+    .catch( error => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No ha sido posible enviar el mansaje!',
+        })
+    } )
+}
+
+
+/* document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
     let nombre = document.getElementById('nombre').value;
@@ -14,4 +37,4 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     
     alert('¡Gracias por tu mensaje! Te contactaremos pronto.');
     this.reset();
-});
+}); */
